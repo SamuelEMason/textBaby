6f61e932459cf7f389f3e9604b981b846e1045af
sam <subterranean.layer@gmail.com>
2023-08-10 17:01:54 -0400

    cors header edit

--- server/index.js
+++ server/index.js
@@ -35,6 +35,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('common'));
 
 if (process.env.NODE_ENV === 'production') {
+	app.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		next();
+	});
 	app.use(
 		cors({
 			origin: 'https://text-baby-client.vercel.app/',
